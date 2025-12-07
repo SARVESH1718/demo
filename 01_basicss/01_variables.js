a6c515912716343f13ac0e29a913ea6c10db8b5c
MALLIKARJUN MADAWAL <137301903+SARVESH1718@users.noreply.github.com>
2025-12-07 07:32:07 +0000

    datatypes

---
+++ 01_basicss/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountEmail = "sarvesh@gmail.com";
+var accountPassword = "password123";
+accountCity = "Belgaum";
+
+// accountId = 4 // not allowed 
+// accountEmail = "sarvv@gmail.com" //allowed
+
+
+console.log(accountEmail);
+console.table([accountEmail, accountId, accountPassword])
---
+++ 01_basicss/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// treat all js code as newer version
+/*
+
+*/
+// alert("hello") // we're using node.js and not browser
+
+console.log(3 + 3);
+console.log("sarvesh");
+
+let name = "sarvesh"
+let age = 21
+let isLoggedIn = false
+
+// ----------PREMITIVE DATATYPES----------
+// number = 2 to the power 53
+// bigint
+// string = ""
+// boolean = true/false
+// float = 3.32323
+// null ==> is standalone value whichh can be assigned to a variable
+// undefined ==> value not assigned yet
+// symbol ==> unique
+
+
+// ----------OBJECTS----------
+
+console.log(typeof(age));
+console.log(typeof(undefined));
+console.log(typeof null); //null is an object  
+
+
